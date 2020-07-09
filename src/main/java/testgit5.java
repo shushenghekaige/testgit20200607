6124ae17eebf2570f2877384741fb964ef55298d
hkg <13636389663@163.com>
2020-07-10 00:40:05 +0800

    Merge branch '20200709gittest2' into 20200709test1
    # Conflicts:
    #	src/main/java/testgit5.java

--- src/main/java/testgit5.java
+++ src/main/java/testgit5.java
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-public class testgit5 {    public static void main(String[] args) {        System.out.println("jjjjjj");    }}
-=======
-public class testgit5 {    public static void main(String[] args) {        System.out.println("hhhhhhh");    }}
->>>>>>> 20200709gittest2
+public class testgit5 {    public static void main(String[] args) {        System.out.println("hhhhhhh");    }}
